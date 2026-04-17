bf487a3d02e343326e05016f1974fb441c067f9c
zainftw <zainuathar@hotmail.com>
2026-04-17 15:28:31 +0500

    Add dummy SECRET_KEY_BASE for asset precompilation on Render

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,7 +2,10 @@
 # exit on error
 set -o errexit
 
+export SECRET_KEY_BASE=dummy
+
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
+
